fdf08a4c6f81c80740fd28946c1bd059c65dbcd5
Gustav Häger <hager.gustav@gmail.com>
2013-11-11 14:42:12 +0100

    Hepp

--- Graphics/WorldGen.hpp
+++ Graphics/WorldGen.hpp
@@ -1,13 +1,18 @@
 #ifndef WORLD_GEN_H
 #define WORLD_GEN_H
 
+#include <vector>
+#include <math.h>
+
+#include <QVector3D>
+
 #include "Model.hpp"
+#include "simplexnoise1234.h"
 
 class WorldGen{
-public:
-	WorldGen();
-
-	Model* generateWorld(float xRange, float zRange, float xPeriod = 1, float zPeriod = 1, float yScale = 1);
+	public:
+		WorldGen();
+		Model* generateWorld(float xRange, float zRange, float vertexDensity, float octaves[], float yScales[], int nOctaves);
 };
 
 #endif
