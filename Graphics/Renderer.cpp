90eb35b1c3b52f0fd71a395d281e44ca376fe7b6
TiXiR <matti166@student.liu.se>
2014-01-11 17:11:46 +0100

    report

--- Graphics/Renderer.cpp
+++ Graphics/Renderer.cpp
@@ -124,8 +124,8 @@ void Renderer::initFBO(FBO* fbo)
     glGenTextures(1, &fbo->depthTex);
     glBindTexture(GL_TEXTURE_2D, fbo->depthTex);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, shadowMapSize, shadowMapSize, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, 0L);
-    //GLfloat borderColor[4]={1.0,1.0,1.0,1.0};
-    GLfloat borderColor[4]={0.0,0.0,0.0,0.0};
+    GLfloat borderColor[4]={1.0,1.0,1.0,1.0};
+    //GLfloat borderColor[4]={0.0,0.0,0.0,0.0};
     glTexParameterfv(GL_TEXTURE_2D,GL_TEXTURE_BORDER_COLOR, borderColor);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
